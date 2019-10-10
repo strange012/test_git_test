72586ba733a3ebbe60cb23007242dd0717863238
strange012 <javamexx@rambler.ru>
2019-10-10 16:00:26 +0300

    fixed output

--- fib.py
+++ fib.py
@@ -1,13 +1,15 @@
 import sys
 
 def recfib(n):
+    if n < 1:
+        return (0, 0)
     if n == 1:
         return (0, 1)
     a, b = recfib(n-1)
     return b, a + b
 
 def main(argv):
-    return recfib(int(argv[1]))
+    return recfib(int(argv[1]))[1]
     
 if __name__ == '__main__':
     main(sys.argv)
